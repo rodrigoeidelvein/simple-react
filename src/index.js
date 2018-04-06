ed39eeba91c750ea0d96030d7cdd20d20af2145b
Rodrigo Fazenda <rodrigoeidelvein@gmail.com>
2018-04-06 00:31:46 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyCWk-evPQkt0eiIqfR-4mmQmsbnaD7PLwk';
+
+const App = function() {
+  return <div>Hi, Rodrigo</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
